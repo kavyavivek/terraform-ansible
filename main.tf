99d3b5f1407c08bd7733e3a9b6003da5ce0690d3
Kavya <kvivek1986@gmail.com>
2022-07-03 17:01:21 +0000

    new

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-sl-varun88"
+  bucket = "my-tf-test-bucket-sl-kavya241108"
 
   tags = {
     Name        = "My bucket"
